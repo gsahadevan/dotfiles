52d891aa54220acdab3904b566cf94d1c5430275
Gauthaman Sahadevan <gsahadevan@outlook.com>
2022-12-13 18:21:38 +0100

    feat: removed lualine from NvimTree

--- .config/nvim/after/plugin/cmp.rc.lua
+++ .config/nvim/after/plugin/cmp.rc.lua
@@ -4,29 +4,37 @@ if (not status) then return end
 local lspkind = require 'lspkind'
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true
+        }),
     }),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
-  }),
-  formatting = {
-    -- fields = { "kind", "abbr", "menu" }, -- changing the order of appearance
-    format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
-  }
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+    }),
+    formatting = {
+        -- fields = { "kind", "abbr", "menu" }, -- changing the order of appearance
+        format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
+    },
+    window = {
+        completion = {
+            border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+        },
+        documentation = {
+            border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+        },
+    },
 })
 
 vim.cmd [[
--- .config/nvim/after/plugin/lualine.rc.lua
+++ .config/nvim/after/plugin/lualine.rc.lua
@@ -3,94 +3,95 @@ if (not status) then return end
 
 
 local diagnostics = {
-	'diagnostics',
-	-- sources = { 'nvim_diagnostics' },
-	--:wq
-	--sections = { 'error', 'warn', 'info', 'hint' },
-	symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
-	update_in_insert = false,
-	always_visible = true,
+    'diagnostics',
+    -- sources = { 'nvim_diagnostics' },
+    --:wq
+    --sections = { 'error', 'warn', 'info', 'hint' },
+    symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
+    update_in_insert = false,
+    always_visible = true,
 }
 
 local filename = {
-	'filename',
-	file_status = true, -- displays file status (readonly or modified)
-	path = 1, -- 0 = only filename | 1 = relative path | 2 = absolute path
+    'filename',
+    file_status = true, -- displays file status (readonly or modified)
+    path = 1, -- 0 = only filename | 1 = relative path | 2 = absolute path
 }
 
 local location = { 'location' }
 
 local diff = {
-	'diff',
-	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-	cond = hide_in_width,
+    'diff',
+    symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+    cond = hide_in_width,
 }
 
 local spaces = function()
-	return 'spaces: ' .. vim.api.nvim_buf_get_option(0, 'shiftwidth')
+    return 'spaces: ' .. vim.api.nvim_buf_get_option(0, 'shiftwidth')
 end
 
 local encoding = { 'encoding' }
 local filetype = { 'filetype' }
 
 local progress_minimap = function()
-	local current_line = vim.fn.line('.')
-	local total_lines = vim.fn.line('$')
-	local chars = { '__', '▁▁', '▂▂', '▃▃', '▄▄', '▅▅', '▆▆', '▇▇', '██' }
-	local line_ratio = current_line / total_lines
-	local index = math.ceil(line_ratio * #chars)
-	return chars[index]
+    local current_line = vim.fn.line('.')
+    local total_lines = vim.fn.line('$')
+    local chars = { '__', '▁▁', '▂▂', '▃▃', '▄▄', '▅▅', '▆▆', '▇▇', '██' }
+    local line_ratio = current_line / total_lines
+    local index = math.ceil(line_ratio * #chars)
+    return chars[index]
 end
 
 require('lualine').setup {
-	options = {
-		icons_enabled = true,
-		theme = 'auto',
-		-- component_separators = { left = '', right = ''},
-		-- section_separators = { left = '', right = ''},
-		component_separators = { left = '', right = '' },
-		section_separators = { left = '', right = '' },
-		disabled_filetypes = {
-			statusline = {},
-			winbar = {},
-		},
-		ignore_focus = {},
-		always_divide_middle = true,
-		globalstatus = false,
-		refresh = {
-			statusline = 1000,
-			tabline = 1000,
-			winbar = 1000,
-		}
-	},
-	-- override default param
-	-- sections = {
-	-- lualine_a = {'mode'},
-	-- lualine_b = {'branch', 'diff', 'diagnostics'},
-	-- lualine_c = {'filename'},
-	-- lualine_x = {'encoding', 'fileformat', 'filetype'},
-	-- lualine_y = {'progress'},
-	-- lualine_z = {'location'}
-	-- },
-	sections = {
-		lualine_a = { 'mode' },
-		lualine_b = { 'branch', diff },
-		lualine_c = { filename },
-		lualine_x = { diagnostics, spaces, encoding, filetype },
-		lualine_y = { location },
-		lualine_z = { progress_minimap }
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		-- lualine_c = {'filename'},
-		lualine_c = { filename },
-		lualine_x = {},
-		lualine_y = { location },
-		lualine_z = {}
-	},
-	tabline = {},
-	winbar = {},
-	inactive_winbar = {},
-	extensions = {}
+    options = {
+        icons_enabled = true,
+        theme = 'auto',
+        -- component_separators = { left = '', right = ''},
+        -- section_separators = { left = '', right = ''},
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+            'packer', 'NvimTree',
+            statusline = {},
+            winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+        }
+    },
+    -- override default param
+    -- sections = {
+    -- lualine_a = {'mode'},
+    -- lualine_b = {'branch', 'diff', 'diagnostics'},
+    -- lualine_c = {'filename'},
+    -- lualine_x = {'encoding', 'fileformat', 'filetype'},
+    -- lualine_y = {'progress'},
+    -- lualine_z = {'location'}
+    -- },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', diff },
+        lualine_c = { filename },
+        lualine_x = { diagnostics, spaces, encoding, filetype },
+        lualine_y = { location },
+        lualine_z = { progress_minimap }
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        -- lualine_c = {'filename'},
+        lualine_c = { filename },
+        lualine_x = {},
+        lualine_y = { location },
+        lualine_z = {}
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {}
 }
