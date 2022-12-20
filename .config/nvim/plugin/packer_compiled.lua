5aac0d3c59244a879841f6c8163f12a856a5c687
Gauthaman Sahadevan <gsahadevan@outlook.com>
2022-12-20 08:01:17 +0100

    feat(keymaps, lspsaga): add def setup lspsaga, add basic keymaps

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -228,10 +228,20 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/gauthamansahadevan/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
     url = "https://github.com/folke/tokyonight.nvim"
+  },
+  ["which-key.nvim"] = {
+    config = { "\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14which-key\frequire\0" },
+    loaded = true,
+    path = "/Users/gauthamansahadevan/.local/share/nvim/site/pack/packer/start/which-key.nvim",
+    url = "https://github.com/folke/which-key.nvim"
   }
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: which-key.nvim
+time([[Config for which-key.nvim]], true)
+try_loadstring("\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14which-key\frequire\0", "config", "which-key.nvim")
+time([[Config for which-key.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
