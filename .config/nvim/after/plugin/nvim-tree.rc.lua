88199a634d459652a4addcc6ec338ca879019b2e
Gauthaman Sahadevan <gsahadevan@outlook.com>
2022-12-20 07:57:40 +0100

    feat(nvim-tree): add def setup args, close nvim if last remaining window

--- .config/nvim/after/plugin/nvim-tree.rc.lua
+++ .config/nvim/after/plugin/nvim-tree.rc.lua
@@ -11,6 +11,14 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
+    disable_netrw = true,
+    hijack_netrw = true,
+    open_on_setup = true, --
+    ignore_ft_on_setup = { 'dashboard' },
+    -- auto_close = true, -- deprecated
+    open_on_tab = true,
+    hijack_cursor = true,
+    update_cwd = true,
     update_focused_file = {
         enable = true,
         update_cwd = true,
@@ -49,7 +57,9 @@ nvim_tree.setup {
         icons = { error = '', warning = '', info = '', hint = '' },
     },
     view = {
-        width = 30,
+        width = 35,
+        hide_root_folder = true,
+        auto_resize = false,
         side = "left",
         mappings = {
             list = {
@@ -60,3 +70,15 @@ nvim_tree.setup {
         },
     },
 }
+
+-- To close nvim if nvim-tree is the last remaining window
+-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close
+-- Since auto_close is deprecated using the naive solution given in the above link
+vim.api.nvim_create_autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+        if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+            vim.cmd "quit"
+        end
+    end
+})
