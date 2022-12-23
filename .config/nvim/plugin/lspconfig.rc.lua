3065340355e2927719f0cc8acc3a96efd8729a06
Gauthaman Sahadevan <gsahadevan@outlook.com>
2022-12-23 17:50:35 +0100

    fix: spelling mistake on nvim_command

--- .config/nvim/plugin/lspconfig.rc.lua
+++ .config/nvim/plugin/lspconfig.rc.lua
@@ -9,7 +9,7 @@ local on_attach = function(client, bufnr)
     if client.server_capabilities.documentFormattingProvider then
         vim.api.nvim_command [[augroup Format]]
         vim.api.nvim_command [[autocmd! * <buffer>]]
-        vim.api.nvimcommand [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+        vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
         vim.api.nvim_command [[augroup END]]
     end
 end
