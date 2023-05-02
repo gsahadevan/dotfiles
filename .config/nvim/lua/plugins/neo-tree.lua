8c01ae9af2c637a35898d9cfe901b622f8587832
gsahadevan <gsahadevan@outlook.com>
2023-05-02 23:26:47 +0200

    add blank space above neo-tree folder

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -52,52 +52,52 @@ return {
         -- in the form 'LspDiagnosticsSignWarning'
 
         require('neo-tree').setup({
-            add_blank_line_at_top = false, -- Add a blank line at the top of the tree.
+            add_blank_line_at_top = true, -- Add a blank line at the top of the tree.
             auto_clean_after_session_restore = true,
             close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
             source_selector = {
-                winbar = false, -- toggle to show selector on winbar
+                winbar = true, -- toggle to show selector on winbar
                 statusline = false, -- toggle to show selector on statusline
-                show_scrolled_off_parent_node = false, -- this will replace the tabs with the parent path
+                -- show_scrolled_off_parent_node = false, -- this will replace the tabs with the parent path
                 -- of the top visible node when scrolled down.
                 sources = {
-                    { source = "filesystem", display_name = "File" },
-                    { source = "buffers", display_name = "Bufs" },
-                    { source = "git_status", display_name = "Git" },
-                    -- { source = "diagnostics", display_name = "Diagnostic" },
-                    { source = "document_symbols", display_name = "Symbols" },
+                    { source = 'filesystem', display_name = 'File' },
+                    { source = 'buffers', display_name = 'Bufs' },
+                    { source = 'git_status', display_name = 'Git' },
+                    -- { source = 'diagnostics', display_name = 'Diagnostic' },
+                    { source = 'document_symbols', display_name = 'Symbols' },
                 },
-                content_layout = "start", -- only with `tabs_layout` = "equal", "focus"
+                content_layout = 'center', -- only with `tabs_layout` = 'equal', 'focus'
                 --                start  : |/ 裡 bufname     \/...
                 --                end    : |/     裡 bufname \/...
                 --                center : |/   裡 bufname   \/...
-                tabs_layout = "equal", -- start, end, center, equal, focus
+                tabs_layout = 'equal', -- start, end, center, equal, focus
                 --             start  : |/  a  \/  b  \/  c  \            |
                 --             end    : |            /  a  \/  b  \/  c  \|
                 --             center : |      /  a  \/  b  \/  c  \      |
                 --             equal  : |/    a    \/    b    \/    c    \|
                 --             active : |/  focused tab    \/  b  \/  c  \|
-                truncation_character = "…", -- character to use when truncating the tab label
+                truncation_character = '…', -- character to use when truncating the tab label
                 tabs_min_width = nil, -- nil | int: if int padding is added based on `content_layout`
                 tabs_max_width = nil, -- this will truncate text even if `text_trunc_to_fit = false`
                 padding = 0, -- can be int or table
                 -- padding = { left = 2, right = 0 },
-                -- separator = "▕", -- can be string or table, see below
-                separator = { left = "▏", right = "▕" },
-                -- separator = { left = "/", right = "\\", override = nil },     -- |/  a  \/  b  \/  c  \...
-                -- separator = { left = "/", right = "\\", override = "right" }, -- |/  a  \  b  \  c  \...
-                -- separator = { left = "/", right = "\\", override = "left" },  -- |/  a  /  b  /  c  /...
-                -- separator = { left = "/", right = "\\", override = "active" },-- |/  a  / b:active \  c  \...
-                -- separator = "|",                                              -- ||  a  |  b  |  c  |...
+                -- separator = '▕', -- can be string or table, see below
+                separator = { left = '▏', right = '▕' },
+                -- separator = { left = '/', right = '\\', override = nil },     -- |/  a  \/  b  \/  c  \...
+                -- separator = { left = '/', right = '\\', override = 'right' }, -- |/  a  \  b  \  c  \...
+                -- separator = { left = '/', right = '\\', override = 'left' },  -- |/  a  /  b  /  c  /...
+                -- separator = { left = '/', right = '\\', override = 'active' },-- |/  a  / b:active \  c  \...
+                -- separator = '|',                                              -- ||  a  |  b  |  c  |...
                 separator_active = nil, -- set separators around the active tab. nil falls back to `source_selector.separator`
                 show_separator_on_edge = false,
                 --                       true  : |/    a    \/    b    \/    c    \|
                 --                       false : |     a    \/    b    \/    c     |
-                highlight_tab = "NeoTreeTabInactive",
-                highlight_tab_active = "NeoTreeTabActive",
-                highlight_background = "NeoTreeTabInactive",
-                highlight_separator = "NeoTreeTabSeparatorInactive",
-                highlight_separator_active = "NeoTreeTabSeparatorActive",
+                highlight_tab = 'NeoTreeTabInactive',
+                highlight_tab_active = 'NeoTreeTabActive',
+                highlight_background = 'NeoTreeTabInactive',
+                highlight_separator = 'NeoTreeTabSeparatorInactive',
+                highlight_separator_active = 'NeoTreeTabSeparatorActive',
             },
             popup_border_style = 'rounded',
             enable_git_status = true,
@@ -115,7 +115,7 @@ return {
             default_component_configs = {
                 container = {
                     enable_character_fade = true,
-                    width = "100%",
+                    width = '100%',
                     right_padding = 0,
                 },
                 indent = {
