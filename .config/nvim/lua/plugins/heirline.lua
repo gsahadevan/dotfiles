f60e9d8f0c1f56314fddd0c2336f17d31fb71680
gsahadevan <gsahadevan@outlook.com>
2023-05-08 01:26:47 +0200

    add heirline

---
+++ .config/nvim/lua/plugins/heirline.lua
@@ -0,0 +1,9 @@
+return {
+    'rebelot/heirline.nvim',
+    -- You can optionally lazy-load heirline on UiEnter
+    -- to make sure all required plugins and colorschemes are loaded before setup
+    -- event = 'UiEnter',
+    -- config = function()
+    --     require('heirline').setup({ ... })
+    -- end
+}
