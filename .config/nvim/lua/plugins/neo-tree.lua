b7b2de208681ca8b1749689df5432a46687070b0
Gauthaman Sahadevan <gsahadevan@outlook.com>
2023-05-15 12:54:42 +0200

    add icons for neo-tree, though not required

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -177,6 +177,29 @@ return {
             -- see `:h neo-tree-global-custom-commands`
             commands = {},
 
+            -- not required since nvim-web-devicons are used
+            --
+            -- document_symbols = {
+            --     kinds = {
+            --         File = { icon = "󰈙", hl = "Tag" },
+            --         Namespace = { icon = "󰌗", hl = "Include" },
+            --         Package = { icon = "󰏖", hl = "Label" },
+            --         Class = { icon = "󰌗", hl = "Include" },
+            --         Property = { icon = "󰆧", hl = "@property" },
+            --         Enum = { icon = "󰒻", hl = "@number" },
+            --         Function = { icon = "󰊕", hl = "Function" },
+            --         String = { icon = "󰀬", hl = "String" },
+            --         Number = { icon = "󰎠", hl = "Number" },
+            --         Array = { icon = "󰅪", hl = "Type" },
+            --         Object = { icon = "󰅩", hl = "Type" },
+            --         Key = { icon = "󰌋", hl = "" },
+            --         Struct = { icon = "󰌗", hl = "Type" },
+            --         Operator = { icon = "󰆕", hl = "Operator" },
+            --         TypeParameter = { icon = "󰊄", hl = "Type" },
+            --         StaticMethod = { icon = '󰠄 ', hl = 'Function' },
+            --     }
+            -- },
+
             window = {
                 position = 'left',
                 width = 55,
