b11bb21cc886a999a76ece9729f06dad96997493
Gauthaman Sahadevan <gsahadevan@outlook.com>
2023-05-22 00:17:38 +0200

    remove keymaps-lsp.lua

--- .config/nvim/lua/gsahadevan/keymaps.lua
+++ .config/nvim/lua/gsahadevan/keymaps.lua
@@ -4,132 +4,167 @@ local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 -- Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+keymap('', '<Space>', '<Nop>', opts)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- Modes
---   normal_mode = "n",
---   insert_mode = "i",
---   visual_mode = "v",
---   visual_block_mode = "x",
---   term_mode = "t",
---   command_mode = "c",
+--   normal_mode = 'n',
+--   insert_mode = 'i',
+--   visual_mode = 'v',
+--   visual_block_mode = 'x',
+--   term_mode = 't',
+--   command_mode = 'c',
 
 -- Normal --
 -- Select all
-keymap("n", '<C-a>', 'gg<S-v>G', opts)
+keymap('n', '<C-a>', 'gg<S-v>G', opts)
 
 -- Remove search highlighting
-keymap("n", '<leader>nh', ':nohl<CR>', opts)
+keymap('n', '<leader>nh', ':nohl<CR>', opts)
 
 -- Save with root permission (not working for now)
 -- vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
 -- New tab | opting for buffers instead of tabs
--- keymap("n", 'te', ':tabedit<CR>', opts)
--- keymap("n", 'tt', ':tabnew<CR>', opts)
--- keymap("n", 'tp', ':tabprevious<CR>', opts)
--- keymap("n", 'tn', ':tabnext<CR>', opts)
--- keymap("n", 'tc', ':tabclose<CR>', opts)
+-- keymap('n', 'te', ':tabedit<CR>', opts)
+-- keymap('n', 'tt', ':tabnew<CR>', opts)
+-- keymap('n', 'tp', ':tabprevious<CR>', opts)
+-- keymap('n', 'tn', ':tabnext<CR>', opts)
+-- keymap('n', 'tc', ':tabclose<CR>', opts)
 
 -- Split window
-keymap("n", 'ss', ':split<Return><C-w>w', opts)
-keymap("n", 'sv', ':vsplit<Return><C-w>w', opts)
-keymap("n", 'se', '<C-w>=', opts)
-keymap("n", 'sw', ':close<CR>', opts)
+keymap('n', 'ss', ':split<Return><C-w>w', opts)
+keymap('n', 'sv', ':vsplit<Return><C-w>w', opts)
+keymap('n', 'se', '<C-w>=', opts)
+keymap('n', 'sw', ':close<CR>', opts)
 
 -- Increment and Decrement numbers
-keymap("n", '<leader>+', '<C-a>', opts)
-keymap("n", '<leader>-', '<C-x>', opts)
+keymap('n', '<leader>+', '<C-a>', opts)
+keymap('n', '<leader>-', '<C-x>', opts)
 
 -- Move window
--- keymap.set("n", '<Space>', '<C-w>w')
+-- keymap.set('n', '<Space>', '<C-w>w')
 -- keymap.set('n', 'sh', '<C-w>h')
 -- keymap.set('n', 'sk', '<C-w>k')
 -- keymap.set('n', 'sj', '<C-w>j')
 -- keymap.set('n', 'sl', '<C-w>l')
 
 -- Resize window
--- keymap.set("n", '<C-w><left>', '<C-w><')
--- keymap.set("n", '<C-w><right>', '<C-w>>')
--- keymap.set("n", '<C-w><up>', '<C-w>+')
--- keymap.set("n", '<C-w><down>', '<C-w>-')
+-- keymap.set('n', '<C-w><left>', '<C-w><')
+-- keymap.set('n', '<C-w><right>', '<C-w>>')
+-- keymap.set('n', '<C-w><up>', '<C-w>+')
+-- keymap.set('n', '<C-w><down>', '<C-w>-')
 
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
--- keymap("n", "<C-j>", "<C-w>j", opts) -- does not work
--- keymap("n", "<C-k>", "<C-w>k", opts) -- does not work
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap('n', '<C-h>', '<C-w>h', opts)
+-- keymap('n', '<C-j>', '<C-w>j', opts) -- does not work
+-- keymap('n', '<C-k>', '<C-w>k', opts) -- does not work
+keymap('n', '<C-l>', '<C-w>l', opts)
 
 -- Resize with arrows | mapped with workspaces navigation
--- keymap("n", "<C-Up>", ":resize -1<CR>", opts)
--- keymap("n", "<C-Down>", ":resize +3<CR>", opts)
--- keymap("n", "<C-Left>", ":vertical resize -1<CR>", opts)
--- keymap("n", "<C-Right>", ":vertical resize +3<CR>", opts)
+-- keymap('n', '<C-Up>', ':resize -1<CR>', opts)
+-- keymap('n', '<C-Down>', ':resize +3<CR>', opts)
+-- keymap('n', '<C-Left>', ':vertical resize -1<CR>', opts)
+-- keymap('n', '<C-Right>', ':vertical resize +3<CR>', opts)
 
 -- Navigate buffers
--- keymap("n", "<S-l>", ":bnext<CR>", opts)
--- keymap("n", "<S-h>", ":bprevious<CR>", opts)
+-- keymap('n', '<S-l>', ':bnext<CR>', opts)
+-- keymap('n', '<S-h>', ':bprevious<CR>', opts)
 
 -- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+2<CR>==gi", opts)
-keymap("n", "<A-k>", "<Esc>:m .-1<CR>==gi", opts)
+keymap('n', '<A-j>', '<Esc>:m .+2<CR>==gi', opts)
+keymap('n', '<A-k>', '<Esc>:m .-1<CR>==gi', opts)
 
 -- Insert --
 -- Press jk fast to exit insert mode
--- keymap("i", "jk", "<ESC>", opts)
--- keymap("i", "kj", "<ESC>", opts)
+-- keymap('i', 'jk', '<ESC>', opts)
+-- keymap('i', 'kj', '<ESC>', opts)
 
 -- Visual --
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+keymap('v', '<', '<gv', opts)
+keymap('v', '>', '>gv', opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+2<CR>==", opts)
-keymap("v", "<A-k>", ":m .-1<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
+keymap('v', '<A-j>', ':m .+2<CR>==', opts)
+keymap('v', '<A-k>', ':m .-1<CR>==', opts)
+keymap('v', 'p', '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":move '>+2<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-1<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+2<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-1<CR>gv-gv", opts)
+keymap('x', 'J', ":move '>+2<CR>gv-gv", opts)
+keymap('x', 'K', ":move '<-1<CR>gv-gv", opts)
+keymap('x', '<A-j>', ":move '>+2<CR>gv-gv", opts)
+keymap('x', '<A-k>', ":move '<-1<CR>gv-gv", opts)
 
 -- Diagnostics --
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
 -- Terminal --
 -- Better terminal navigation
 -- local term_opts = { silent = true }
--- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
--- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
--- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
--- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+-- keymap('t', '<C-h>', '<C-\\><C-N><C-w>h', term_opts)
+-- keymap('t', '<C-j>', '<C-\\><C-N><C-w>j', term_opts)
+-- keymap('t', '<C-k>', '<C-\\><C-N><C-w>k', term_opts)
+-- keymap('t', '<C-l>', '<C-\\><C-N><C-w>l', term_opts)
 
 
 -- the primeagen
--- vim.keymap.set('v', 'J', ":m '>+2<CR>gv=gv")
--- vim.keymap.set('v', 'K', ":m '<-1<CR>gv=gv")
+-- vim.keymap.set('v', 'J', ':m '>+2<CR>gv=gv')
+-- vim.keymap.set('v', 'K', ':m '<-1<CR>gv=gv')
 
-vim.keymap.set("n", "J", "mzJ`z")          -- keeps cursor in place when joining lines
-vim.keymap.set("n", "<C-d>", "<C-d>zz")    -- keeps cursor in the middle of screen
-vim.keymap.set("n", "<C-u>", "<C-u>zz")    -- keeps cursor in the middle of screen
-vim.keymap.set("n", "n", "nzzzv")          -- keeps cursor in the middle for search terms
-vim.keymap.set("n", "N", "Nzzzv")          -- keeps cursor in the middle for search terms
-vim.keymap.set("x", "<leader>pp", "\"_dp") -- preserve pasted in buffer
-vim.keymap.set("n", "x", '"_x')            -- do not save characters cut using x
+vim.keymap.set('n', 'J', 'mzJ`z')          -- keeps cursor in place when joining lines
+vim.keymap.set('n', '<C-d>', '<C-d>zz')    -- keeps cursor in the middle of screen
+vim.keymap.set('n', '<C-u>', '<C-u>zz')    -- keeps cursor in the middle of screen
+vim.keymap.set('n', 'n', 'nzzzv')          -- keeps cursor in the middle for search terms
+vim.keymap.set('n', 'N', 'Nzzzv')          -- keeps cursor in the middle for search terms
+vim.keymap.set('x', '<leader>pp', '\'_dp') -- preserve pasted in buffer
+vim.keymap.set('n', 'x', '"_x')            -- do not save characters cut using x
 
 -- pressing leaderY would enabled further yanking to save yanked text to clipboard
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set('n', '<leader>y', '\'+y')
+vim.keymap.set('v', '<leader>y', '\'+y')
+vim.keymap.set('n', '<leader>Y', '\'+Y')
 
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set('n', '<leader>d', '\'_d')
+vim.keymap.set('v', '<leader>d', '\'_d')
+vim.keymap.set('v', '<leader>d', '\'_d')
+
+-- code navigation shortcuts, check :help lsp for more details
+-- local opts = { noremap = true, silent = true }
+-- local keymap = vim.api.nvim_set_keymap
+-- keymap('n', 'K', ':lua vim.lsp.buf.hover()<CR>', opts)
+-- keymap('n', '<c-k>', ':lua vim.lsp.buf.signature_help()<CR>', opts)
+-- keymap('n', '<c-]>', ':lua vim.lsp.buf.declaration()<CR>', opts)
+-- keymap('n', 'gd', ':lua vim.lsp.buf.definition()<CR>', opts)
+-- keymap('n', 'gD', ':lua vim.lsp.buf.type_definition()<CR>', opts)
+-- keymap('n', 'gr', ':lua vim.lsp.buf.references()<CR>', opts)
+-- keymap('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>', opts)
+-- keymap('n', 'g0', ':lua vim.lsp.buf.document_symbol()<CR>', opts)
+-- keymap('n', 'gW', ':lua vim.lsp.buf.workspace_symbol()<CR>', opts)
+-- code actions
+-- keymap('n', '<leader>ca', ':lua vim.lsp.buf.code_action()<CR>', opts)
+
+-- goto previous/next diagnostic warning/error
+-- keymap('n', '[d', ':lua vim.diagnostic.goto_prev()<CR>', opts)
+-- keymap('n', ']d', ':lua vim.diagnostic.goto_next()<CR>', opts)
+-- keymap('n', 'gl', ':lua vim.diagnostic.open_float()<CR>', opts)
+
+-- local set_hl_for_floating_window = function()
+--     vim.api.nvim_set_hl(0, 'NormalFloat', {
+--         link = 'Normal',
+--     })
+--     vim.api.nvim_set_hl(0, 'FloatBorder', {
+--         bg = 'none',
+--     })
+-- end
+-- set_hl_for_floating_window()
+-- vim.api.nvim_create_autocmd('ColorScheme', {
+--     pattern = '*',
+--     desc = 'Avoid overwritten by loading color schemes later',
+--     callback = set_hl_for_floating_window,
+-- })
