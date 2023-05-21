2f9a6be586944640bdf4d772893540a54a5287e8
Gauthaman Sahadevan <gsahadevan@outlook.com>
2023-05-22 00:01:54 +0200

    move lspsaga to lazy

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -6,6 +6,5 @@ return {
     { 'williamboman/mason-lspconfig.nvim' }, -- closes gaps between mason and lspconfig
     { 'neovim/nvim-lspconfig' },             -- configurations for neovim LSP
     { 'jose-elias-alvarez/null-ls.nvim' },   -- use neovim as language server to inject LSP diagnostics, code actions, and more via lua
-    { 'glepnir/lspsaga.nvim' },
     { 'rhysd/git-messenger.vim' },           -- shows history of commits under cursor in a pop window
 }
