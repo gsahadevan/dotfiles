de56f875a5c6d7b14ad4591bf5bb1d5d315492a2
gsahadevan <gsahadevan@outlook.com>
2023-05-27 17:57:15 +0200

    remove lualine evil-line theme

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -182,298 +182,68 @@ local mode = {
 return {
     'nvim-lualine/lualine.nvim',
     config = function()
-        -- require('lualine').setup({
-        --     options = {
-        --         icons_enabled = true,
-        --         theme = 'horizon', -- lualine theme
-        --         component_separators = { left = '', right = '' },
-        --         section_separators = { left = '', right = '' },
-        --
-        --         disabled_filetypes = {
-        --             -- Filetypes to disable lualine for.
-        --             'packer',
-        --             'NvimTree',
-        --             -- 'neo-tree',
-        --             statusline = {}, -- only ignores the ft for statusline.
-        --             winbar = {}, -- only ignores the ft for winbar.
-        --         },
-        --
-        --         ignore_focus = {}, -- If current filetype is in this list it'll
-        --         -- always be drawn as inactive statusline
-        --         -- and the last window will be drawn as active statusline.
-        --         -- for example if you don't want statusline of
-        --         -- your file tree / sidebar window to have active
-        --         -- statusline you can add their filetypes here.
-        --
-        --         always_divide_middle = true, -- When set to true, left sections i.e. 'a','b' and 'c'
-        --         -- can't take over the entire statusline even
-        --         -- if neither of 'x', 'y' or 'z' are present.
-        --
-        --         globalstatus = true, -- enable global statusline (have a single statusline
-        --         -- at bottom of neovim instead of one for  every window).
-        --         -- This feature is only available in neovim 0.7 and higher.
-        --
-        --         refresh = {
-        --             -- sets how often lualine should refresh it's contents (in ms)
-        --             statusline = 1000, -- The refresh option sets minimum time that lualine tries
-        --             tabline = 1000, -- to maintain between refresh. It's not guarantied if situation
-        --             winbar = 1000 -- arises that lualine needs to refresh itself before this time
-        --             -- it'll do it.
-        --
-        --             -- Also you can force lualine's refresh by calling refresh function
-        --             -- like require('lualine').refresh()
-        --         }
-        --     },
-        --     sections = {
-        --         lualine_a = { mode },
-        --         lualine_b = { 'branch' },
-        --         lualine_c = { diff, diagnostics },
-        --         lualine_x = { 'encoding', fileformat, filetype },
-        --         lualine_y = { 'progress' },
-        --         lualine_z = { 'location' },
-        --     },
-        --     inactive_sections = {
-        --         lualine_a = {},
-        --         lualine_b = {},
-        --         lualine_c = { filename },
-        --         lualine_x = {},
-        --         lualine_y = {},
-        --         lualine_z = {},
-        --     },
-        --     tabline = {},
-        --     winbar = {},
-        --     inactive_winbar = {},
-        --     extensions = {}
-        -- })
-        --
-        --
-        -- Eviline config for lualine
-        -- Author: shadmansaleh
-        -- Credit: glepnir
-        local lualine = require('lualine')
-        --
-        -- Color table for highlights
-        -- stylua: ignore
-        local colors = {
-            bg       = '#202328',
-            fg       = '#bbc2cf',
-            yellow   = '#ECBE7B',
-            cyan     = '#008080',
-            darkblue = '#081633',
-            green    = '#98be65',
-            orange   = '#FF8800',
-            violet   = '#a9a1e1',
-            magenta  = '#c678dd',
-            blue     = '#51afef',
-            red      = '#ec5f67',
-        }
-
-        local conditions = {
-            buffer_not_empty = function()
-                return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
-            end,
-            hide_in_width = function()
-                return vim.fn.winwidth(0) > 80
-            end,
-            check_git_workspace = function()
-                local filepath = vim.fn.expand('%:p:h')
-                local gitdir = vim.fn.finddir('.git', filepath .. ';')
-                return gitdir and #gitdir > 0 and #gitdir < #filepath
-            end,
-        }
-
-        -- Config
-        local config = {
+        require('lualine').setup({
             options = {
-                -- Disable sections and component separators
-                component_separators = '',
-                section_separators = '',
-                theme = {
-                    -- We are going to use lualine_c an lualine_x as left and
-                    -- right section. Both are highlighted by c theme .  So we
-                    -- are just setting default looks o statusline
-                    normal = { c = { fg = colors.fg, bg = colors.bg } },
-                    inactive = { c = { fg = colors.fg, bg = colors.bg } },
-                },
+                icons_enabled = true,
+                theme = 'horizon', -- lualine theme
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+
                 disabled_filetypes = {
                     -- Filetypes to disable lualine for.
                     'packer',
+                    'NvimTree',
+                    -- 'neo-tree',
                     statusline = {}, -- only ignores the ft for statusline.
                     winbar = {}, -- only ignores the ft for winbar.
                 },
-                globalstatus = true,
+
+                ignore_focus = {}, -- If current filetype is in this list it'll
+                -- always be drawn as inactive statusline
+                -- and the last window will be drawn as active statusline.
+                -- for example if you don't want statusline of
+                -- your file tree / sidebar window to have active
+                -- statusline you can add their filetypes here.
+
+                always_divide_middle = true, -- When set to true, left sections i.e. 'a','b' and 'c'
+                -- can't take over the entire statusline even
+                -- if neither of 'x', 'y' or 'z' are present.
+
+                globalstatus = true, -- enable global statusline (have a single statusline
+                -- at bottom of neovim instead of one for  every window).
+                -- This feature is only available in neovim 0.7 and higher.
+
+                refresh = {
+                    -- sets how often lualine should refresh it's contents (in ms)
+                    statusline = 1000, -- The refresh option sets minimum time that lualine tries
+                    tabline = 1000, -- to maintain between refresh. It's not guarantied if situation
+                    winbar = 1000 -- arises that lualine needs to refresh itself before this time
+                    -- it'll do it.
+
+                    -- Also you can force lualine's refresh by calling refresh function
+                    -- like require('lualine').refresh()
+                }
             },
             sections = {
-                -- these are to remove the defaults
-                lualine_a = {},
-                lualine_b = {},
-                lualine_y = {},
-                lualine_z = {},
-                -- These will be filled later
-                lualine_c = {},
-                lualine_x = {},
+                lualine_a = { mode },
+                lualine_b = { 'branch' },
+                lualine_c = { diff, diagnostics },
+                lualine_x = { 'encoding', fileformat, filetype },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' },
             },
             inactive_sections = {
-                -- these are to remove the defaults
                 lualine_a = {},
                 lualine_b = {},
+                lualine_c = { filename },
+                lualine_x = {},
                 lualine_y = {},
                 lualine_z = {},
-                lualine_c = {},
-                lualine_x = {},
             },
-        }
-
-        -- Inserts a component in lualine_c at left section
-        local function ins_left(component)
-            table.insert(config.sections.lualine_c, component)
-        end
-
-        -- Inserts a component in lualine_x at right section
-        local function ins_right(component)
-            table.insert(config.sections.lualine_x, component)
-        end
-
-        ins_left {
-            function()
-                return '▊'
-            end,
-            color = { fg = colors.blue }, -- Sets highlighting of component
-            padding = { left = 0, right = 1 }, -- We don't need space before this
-        }
-
-        ins_left {
-            -- mode component
-            function()
-                return ''
-            end,
-            color = function()
-                -- auto change color according to neovims mode
-                local mode_color = {
-                    n = colors.red,
-                    i = colors.green,
-                    v = colors.blue,
-                    [''] = colors.blue,
-                    V = colors.blue,
-                    c = colors.magenta,
-                    no = colors.red,
-                    s = colors.orange,
-                    S = colors.orange,
-                    [''] = colors.orange,
-                    ic = colors.yellow,
-                    R = colors.violet,
-                    Rv = colors.violet,
-                    cv = colors.red,
-                    ce = colors.red,
-                    r = colors.cyan,
-                    rm = colors.cyan,
-                    ['r?'] = colors.cyan,
-                    ['!'] = colors.red,
-                    t = colors.red,
-                }
-                return { fg = mode_color[vim.fn.mode()] }
-            end,
-            padding = { right = 1 },
-        }
-
-        ins_left {
-            -- filesize component
-            'filesize',
-            cond = conditions.buffer_not_empty,
-        }
-
-        ins_left {
-            'filename',
-            cond = conditions.buffer_not_empty,
-            color = { fg = colors.magenta, gui = 'bold' },
-        }
-
-        ins_left { 'location' }
-
-        ins_left { 'progress', color = { fg = colors.fg, gui = 'bold' } }
-
-        ins_left {
-            'diagnostics',
-            sources = { 'nvim_diagnostic' },
-            symbols = { error = ' ', warn = ' ', info = ' ' },
-            diagnostics_color = {
-                color_error = { fg = colors.red },
-                color_warn = { fg = colors.yellow },
-                color_info = { fg = colors.cyan },
-            },
-        }
-
-        -- Insert mid section. You can make any number of sections in neovim :)
-        -- for lualine it's any number greater then 2
-        ins_left {
-            function()
-                return '%='
-            end,
-        }
-
-        ins_left {
-            -- Lsp server name .
-            function()
-                local msg = 'No Active Lsp'
-                local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-                local clients = vim.lsp.get_active_clients()
-                if next(clients) == nil then
-                    return msg
-                end
-                for _, client in ipairs(clients) do
-                    local filetypes = client.config.filetypes
-                    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                        return client.name
-                    end
-                end
-                return msg
-            end,
-            icon = ' LSP:',
-            color = { fg = '#ffffff', gui = 'bold' },
-        }
-
-        -- Add components to right sections
-        ins_right {
-            'o:encoding', -- option component same as &encoding in viml
-            fmt = string.upper, -- I'm not sure why it's upper case either ;)
-            cond = conditions.hide_in_width,
-            color = { fg = colors.green, gui = 'bold' },
-        }
-
-        ins_right {
-            'fileformat',
-            fmt = string.upper,
-            icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
-            color = { fg = colors.green, gui = 'bold' },
-        }
-
-        ins_right {
-            'branch',
-            icon = '',
-            color = { fg = colors.violet, gui = 'bold' },
-        }
-
-        ins_right {
-            'diff',
-            -- Is it me or the symbol for modified us really weird
-            symbols = { added = ' ', modified = '󰝤 ', removed = ' ' },
-            diff_color = {
-                added = { fg = colors.green },
-                modified = { fg = colors.orange },
-                removed = { fg = colors.red },
-            },
-            cond = conditions.hide_in_width,
-        }
-
-        ins_right {
-            function()
-                return '▊'
-            end,
-            color = { fg = colors.blue },
-            padding = { left = 1 },
-        }
-
-        -- Now don't forget to initialize lualine
-        lualine.setup(config)
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {}
+        })
     end
 }
