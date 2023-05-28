a4b46fd34358cf263887cd0cee13f4f811a6a809
gsahadevan <gsahadevan@outlook.com>
2023-05-28 12:50:16 +0200

    move luasnip to lazy

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -4,7 +4,7 @@ local MIN_LABEL_WIDTH = 35
 local icons = require "icons.font-icons"
 
 return {
-    'hrsh7th/nvim-cmp',     -- autocompletion plugin
+    'hrsh7th/nvim-cmp',         -- autocompletion plugin
     dependencies = {
         'hrsh7th/cmp-buffer',   -- completion source for buffer words
         'hrsh7th/cmp-nvim-lua', -- completion source for lua
@@ -73,5 +73,65 @@ return {
                 end,
             },
         })
+
+
+        local luasnip = require('luasnip')
+        local s = luasnip.snippet
+        local t = luasnip.text_node
+        local i = luasnip.insert_node
+        local types = require('luasnip.util.types')
+
+        luasnip.setup({
+            history = true,
+            -- Update more often, :h events for more info.
+            update_events = 'TextChanged,TextChangedI',
+            -- Snippets aren't automatically removed if their text is deleted.
+            -- `delete_check_events` determines on which events (:h events) a check for
+            -- deleted snippets is performed.
+            -- This can be especially useful when `history` is enabled.
+            delete_check_events = 'TextChanged',
+            ext_opts = {
+                [types.choiceNode] = {
+                    active = {
+                        virt_text = { { 'choiceNode', 'Comment' } },
+                    },
+                },
+            },
+            -- treesitter-hl has 100, use something higher (default is 200).
+            ext_base_prio = 300,
+            -- minimal increase in priority.
+            ext_prio_increase = 1,
+            enable_autosnippets = true,
+            -- mapping for cutting selected text so it's usable as SELECT_DEDENT,
+            -- SELECT_RAW or TM_SELECTED_TEXT (mapped via xmap).
+            store_selection_keys = '<Tab>',
+            -- luasnip uses this function to get the currently active filetype. This
+            -- is the (rather uninteresting) default, but it's possible to use
+            -- eg. treesitter for getting the current filetype by setting ft_func to
+            -- require("luasnip.extras.filetype_functions").from_cursor (requires
+            -- `nvim-treesitter/nvim-treesitter`). This allows correctly resolving
+            -- the current filetype in eg. a markdown-code block or `vim.cmd()`.
+            ft_func = function()
+                ---@diagnostic disable-next-line: param-type-mismatch
+                return vim.split(vim.bo.filetype, '.', true)
+            end,
+        })
+
+        luasnip.add_snippets('lua', {
+            s('pt', { t('print('), i(1, '...'), t(')') }),
+            s('ipt', { t('print(vim.inspect({'), i(1, '...'), t('}))') }),
+            s('fn',
+                { t('local '), i(1), t(' = function('), i(2, 'args'), t({ ')', '\t' }), i(3, '...'), t({ '', 'end' }) }),
+            s('mfn', { t('M.'), i(1), t(' = function( '), i(2), t({ ')', '\t' }), i(3), t({ '', 'end' }) }),
+        })
+
+        luasnip.add_snippets('typescript', {
+            s('log', { t('console.log('), i(1), t(')') }),
+            s('err', { t('console.error('), i(1), t(')') }),
+            s('oimp', { t('import { '), i(1), t(' } from "'), i(2), t('"') }),
+            s('exp', { t('export { '), i(1), t(' }"') }),
+        })
+
+        require('luasnip.loaders.from_vscode').lazy_load()
     end
 }
