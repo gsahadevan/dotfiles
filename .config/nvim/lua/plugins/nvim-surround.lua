2b769c784f4eca3af615625814e7cff6dc24efb4
gsahadevan <gsahadevan@outlook.com>
2023-06-12 22:17:45 +0200

    add nvim-bqf and vim-illuminate

--- .config/nvim/lua/plugins/nvim-surround.lua
+++ .config/nvim/lua/plugins/nvim-surround.lua
@@ -10,12 +10,13 @@ return {
 }
 
 -- Some default commands from nvim-surround
---     Old text                    Command         New text
+--     Old text                 Command         New text
 --------------------------------------------------------------------------------
--- surr*ound_words             ysiw)           (surround_words)
--- *make strings               ys$"            "make strings"
--- [delete ar*ound me!]        ds]             delete around me!
--- remove <b>HTML t*ags</b>    dst             remove HTML tags
--- 'change quot*es'            cs'"            "change quotes"
--- <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
--- delete(functi*on calls)     dsf             function calls
+-- surr*ound_words              ysiw)           (surround_words)
+-- *make strings                ys$"            "make strings"
+-- [delete ar*ound me!]         ds]             delete around me!
+-- remove <b>HTML t*ags</b>     dst             remove HTML tags
+-- 'change quot*es'             cs'"            "change quotes"
+-- <b>or tag* types</b>         csth1<CR>       <h1>or tag types</h1>
+-- delete(functi*on calls)      dsf             function calls
+-- surround with function       yswf            surround word with function (prompt to enter function name)
