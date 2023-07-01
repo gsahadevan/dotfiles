7587fc092fa730ea1de1e1e66a075be27d442733
gsahadevan <gsahadevan@outlook.com>
2023-07-01 10:23:30 +0200

    add tpm to tmux, add iterm2 themes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,4 +6,4 @@ require('gsahadevan.plugins-keymaps')
 -- setup must be called before loading
 -- you could also change the colorscheme using telescope <leader>ft and :colorscheme
 -- vim.cmd('colorscheme rose-pine')
-vim.api.nvim_command('colorscheme tokyonight-night')
+vim.api.nvim_command('colorscheme catppuccin-frappe')
