4b29c9ad257ab3a967b218832f73a4c94857c8fc
Gauthaman Sahadevan <gsahadevan@outlook.com>
2023-07-12 20:53:58 +0200

    change mapping for nohlsearch

--- .config/nvim/lua/gsahadevan/keymaps.lua
+++ .config/nvim/lua/gsahadevan/keymaps.lua
@@ -15,8 +15,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 keymap('n', '<leader>a', 'gg<S-v>G', { noremap = true, silent = true, desc = 'Select all text in buffer' })
-keymap('n', '<leader>nh', ':nohl<CR>', { noremap = true, silent = true, desc = 'No (Remove) search highlighting' })
-keymap('n', '<esc>', ':nohl<CR>', { desc = 'No (Remove) search highlighting' })
+keymap('n', '<esc>', ':nohlsearch<CR>', { noremap = true, silent = true, desc = 'No (Remove) search highlighting' })
 
 -- Split window
 keymap('n', 'sv', ':vsplit<Return><C-w>w', { noremap = true, silent = true, desc = 'Window split current vertically' })
