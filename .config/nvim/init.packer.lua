1b96e6a8382968c298025cd9fdeba4ed7a862e8a
Gauthaman Sahadevan <gsahadevan@outlook.com>
2023-10-07 01:19:08 +0200

    feat(nvim.packer): add lspsaga keybindings, remove lsp-zero bindings

--- .config/nvim/init.packer.lua
+++ .config/nvim/init.packer.lua
@@ -485,7 +485,7 @@ local lsp = require('lsp-zero').preset({})
 lsp.on_attach(function(client, bufnr)
     lsp.default_keymaps({
         buffer = bufnr,
-        preserve_mappings = false -- read here for more understanding - https://github.com/VonHeikemen/lsp-zero.nvim#keybindings
+        -- preserve_mappings = false -- read here for more understanding - https://github.com/VonHeikemen/lsp-zero.nvim#keybindings
     })
 end)
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls()) -- (optional) configure lua language server for neovim
@@ -506,28 +506,28 @@ require('lspsaga').setup({
     },
 })
 -- Add keymaps for Lspsaga
-keymap('n', 'gk', '<cmd>Lspsaga hover_doc<cr>', { desc = 'Show hover info of symbol under cursor' })
-keymap('n', 'gh', '<cmd>Lspsaga finder tyd+ref+imp+def<cr>', { desc = 'Show LSP finder' })
-keymap('n', '<leader>o', '<cmd>Lspsaga outline<cr>', { desc = 'Open sysmbols outline' })
-keymap('n', '<leader>ca', '<cmd>Lspsaga code_action<cr>', { desc = 'Show code action available for cursor pos' }) -- changed from <f4>
+keymap('n', 'K', '<cmd>Lspsaga hover_doc<cr>', { nowait = true, noremap = true, silent = true, desc = 'Show hover info of symbol under cursor' })
+keymap('n', 'gh', '<cmd>Lspsaga finder tyd+ref+imp+def<cr>', { noremap = true, silent = true, desc = 'Show LSP finder' })
+keymap('n', '<leader>o', '<cmd>Lspsaga outline<cr>', { noremap = true, silent = true, desc = 'Open sysmbols outline' })
+keymap('n', '<leader>ca', '<cmd>Lspsaga code_action<cr>', { noremap = true, silent = true, desc = 'Show code action available for cursor pos' }) -- changed from <f4>
 -- Lspsaga call heirarchy
-keymap('n', '<leader>ci', '<cmd>Lspsaga incoming_calls<cr>', { desc = 'Show incoming call heirarchy' })
-keymap('n', '<leader>co', '<cmd>Lspsaga outgoing_calls<cr>', { desc = 'Show outgoing call heirarchy' })
+keymap('n', '<leader>ci', '<cmd>Lspsaga incoming_calls<cr>', { noremap = true, silent = true, desc = 'Show incoming call heirarchy' })
+keymap('n', '<leader>co', '<cmd>Lspsaga outgoing_calls<cr>', { noremap = true, silent = true, desc = 'Show outgoing call heirarchy' })
 -- Lspsaga definitions
-keymap('n', 'gd', '<cmd>Lspsaga goto_definition<cr>', { desc = 'Lspsaga goto definition' })
-keymap('n', 'gD', '<cmd>Lspsaga peek_definition<cr>', { desc = 'Lspsaga peek definition' })
-keymap('n', 'gt', '<cmd>Lspsaga goto_type_definition<cr>', { desc = 'Lspsaga goto type definition' })
-keymap('n', 'gT', '<cmd>Lspsaga peek_type_definition<cr>', { desc = 'Lspsaga peek type definition' })
+keymap('n', 'gd', '<cmd>Lspsaga goto_definition<cr>', { nowait = true, noremap = true, silent = true })
+keymap('n', 'gD', '<cmd>Lspsaga peek_definition<cr>', { nowait = true, noremap = true, silent = true, desc = 'Lspsaga peek definition' })
+keymap('n', 'gt', '<cmd>Lspsaga goto_type_definition<cr>', { nowait = true, noremap = true, silent = true, desc = 'Lspsaga goto type definition' })
+keymap('n', 'gT', '<cmd>Lspsaga peek_type_definition<cr>', { nowait = true, noremap = true, silent = true, desc = 'Lspsaga peek type definition' })
 -- Lspsaga diagnostics
-keymap('n', 'sl', '<cmd>Lspsaga show_line_diagnostics<cr>', { desc = 'Show line diagnostics' })
-keymap('n', 'sc', '<cmd>Lspsaga show_cursor_diagnostics<cr>', { desc = 'Show cursor diagnostics' })
-keymap('n', 'sb', '<cmd>Lspsaga show_buf_diagnostics<cr>', { desc = 'Show buffer diagnostics' })
-keymap('n', ']g', '<cmd>Lspsaga diagnostic_jump_prev<cr>', { desc = 'Move to the previous diagnostic in current buffer' })
-keymap('n', '[g', '<cmd>Lspsaga diagnostic_jump_next<cr>', { desc = 'Move to the previous diagnostic in current buffer' })
+keymap('n', 'sl', '<cmd>Lspsaga show_line_diagnostics<cr>', { noremap = true, silent = true, desc = 'Show line diagnostics' })
+keymap('n', 'sc', '<cmd>Lspsaga show_cursor_diagnostics<cr>', { noremap = true, silent = true, desc = 'Show cursor diagnostics' })
+keymap('n', 'sb', '<cmd>Lspsaga show_buf_diagnostics<cr>', { noremap = true, silent = true, desc = 'Show buffer diagnostics' })
+keymap('n', 'g]', '<cmd>Lspsaga diagnostic_jump_prev<cr>', { noremap = true, silent = true, desc = 'Move to the prev diagnostic in current buffer' })
+keymap('n', 'g[', '<cmd>Lspsaga diagnostic_jump_next<cr>', { noremap = true, silent = true, desc = 'Move to the next diagnostic in current buffer' })
 -- Lspsaga misc
-keymap('n', '<leader>cr', '<cmd>Lspsaga rename<cr>', { desc = 'Renames all references of symbol under cursor' })
-keymap('n', '<leader>cf', vim.lsp.buf.format, { desc = 'Format code in current buffer' })
-keymap('n', '<leader>cq', vim.diagnostic.setloclist, { desc = 'Open diagnostic set loc list' })
+keymap('n', '<leader>cr', '<cmd>Lspsaga rename<cr>', { noremap = true, silent = true, desc = 'Renames all references of symbol under cursor' })
+keymap('n', '<leader>cf', vim.lsp.buf.format, { noremap = true, silent = true, desc = 'Format code in current buffer' })
+keymap('n', '<leader>cq', vim.diagnostic.setloclist, { noremap = true, silent = true, desc = 'Open diagnostic set loc list' })
 -- Configure completion
 -- You need to setup `cmp` after lsp-zero
 local ELLIPSIS_CHAR = '…'
