cc26d20121868502d2f48220d9da35b4874d2695
Gauthaman Sahadevan <gsahadevan@outlook.com>
2025-03-31 22:47:20 +0200

    Add autocmds lua to init file

--- .config/nvim.lazy.v2/init.lua
+++ .config/nvim.lazy.v2/init.lua
@@ -8,6 +8,7 @@ require('config.lazy')
 require('config.options')
 require('config.keymaps')
 require('config.commands')
+require('config.autocmds')
 
 -- setup must be called before loading
 vim.api.nvim_command('colorscheme  xcodedark')
