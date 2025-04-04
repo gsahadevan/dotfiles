2d6139d6387cdf4fb322a3cc45730fbbc30943f9
Gauthaman Sahadevan <gsahadevan@outlook.com>
2025-04-04 19:26:03 +0200

    Add known colorschemes

--- .config/nvim/init.lazy.lua
+++ .config/nvim/init.lazy.lua
@@ -299,8 +299,59 @@ require('lazy').setup({
         },
         -- adds indentation guides to all lines (including empty lines), using nvim's virtual text feature
         { 'lukas-reineke/indent-blankline.nvim' },
-        -- primary colorscheme
-        { 'catppuccin/nvim' },
+        -- primary colorscheme catppuccin
+        -- priority = 1000 -> to load it before all the other start plugins
+        {
+            { 'catppuccin/nvim',              name = 'catppuccin',  priority = 1000 },
+            { 'arzg/vim-colors-xcode',        name = 'xcode' },
+            { 'Mofiqul/dracula.nvim',         name = 'dracula' },
+            { 'projekt0n/github-nvim-theme',  name = 'github-theme' },
+            { 'Yazeed1s/minimal.nvim' },
+            { 'lunarvim/horizon.nvim' },
+            { 'rebelot/kanagawa.nvim' },
+            { 'Shatur/neovim-ayu' },
+            { 'frenzyexists/aquarium-vim' },
+            { 'folke/tokyonight.nvim',        lazy = false,         priority = 1000, opts = {} },
+            { 'bluz71/vim-nightfly-colors',   name = 'nightfly',    lazy = false,    priority = 1000 },
+            { 'bluz71/vim-moonfly-colors',    name = 'moonfly',     lazy = false,    priority = 1000 },
+            { 'rose-pine/neovim',             name = 'rose-pine' },
+            { 'AlexvZyl/nordic.nvim',         lazy = false,         priority = 1000, },
+            { 'dgox16/oldworld.nvim',         lazy = false,         priority = 1000 },
+            { 'comfysage/evergarden',         lazy = false,         priority = 1000 },
+            { 'EdenEast/nightfox.nvim' },
+            { 'scottmckendry/cyberdream.nvim' },
+            { 'savq/melange-nvim' },
+            { 'rmehri01/onenord.nvim' },
+            { 'vague2k/vague.nvim' },
+            { 'sainnhe/everforest' },
+            { 'sainnhe/sonokai' },
+            { 'sainnhe/gruvbox-material' },
+            { 'sainnhe/edge' },
+            { 'sainnhe/everforest' },
+            { 'sainnhe/gruvbox-material' },
+            { 'sainnhe/sonokai' },
+            { 'sainnhe/edge' },
+            { 'sainnhe/everforest' },
+            { 'sainnhe/gruvbox-material' },
+            { 'sainnhe/sonokai' },
+            {
+                'ribru17/bamboo.nvim',
+                lazy = false,
+                priority = 1000,
+                config = function()
+                    require('bamboo').setup {
+                        -- optional configuration here
+                    }
+                    require('bamboo').load()
+                end,
+            },
+            {
+                'craftzdog/solarized-osaka.nvim',
+                lazy = false,
+                priority = 1000,
+                opts = {},
+            }
+        },
         -- statusline written in lua
         { 'nvim-lualine/lualine.nvim' },
         -- fuzzy finder for files
